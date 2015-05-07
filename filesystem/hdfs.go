4cb8eb76639de162bd4ae6bd7c996d29b2f3363e
Gang Bai <me@baigang.net>
2015-05-07 14:57:25 +0800

    Added retry in hdfs.Write.

--- filesystem/hdfs.go
+++ filesystem/hdfs.go
@@ -166,12 +166,21 @@ func (f *HdfsFile) Write(b []byte) (int, error) {
 		return 0, fmt.Errorf("Write: NewRequest failed: %v", err)
 	}
 	// no redirect
-	resp, err := tr.RoundTrip(req)
-	if err != nil {
-		return 0, fmt.Errorf("Write: RoundTrip failed: %v", err)
+
+	var resp *http.Response
+	var loc string
+	for retry := 0; retry < 3 && (err != nil || loc == ""); retry++ {
+		resp, err := tr.RoundTrip(req)
+		if err != nil {
+			continue
+		}
+		defer resp.Body.Close()
+		loc = resp.Header.Get("Location")
 	}
-	defer resp.Body.Close()
-	loc := resp.Header.Get("Location")
+
+	if loc == "" {
+		return 0, fmt.Errorf("Write: Failed retrieving datanode location.")
+	}
 
 	u, err := url.ParseRequestURI(loc)
 	if err != nil {
@@ -179,6 +188,9 @@ func (f *HdfsFile) Write(b []byte) (int, error) {
 	}
 	// POST request to datanode.
 	resp, err = http.Post(u.String(), "application/octet-stream", bytes.NewBuffer(b))
+	for retry := 0; retry < 3 && err != nil; retry++ {
+		resp, err = http.Post(u.String(), "application/octet-stream", bytes.NewBuffer(b))
+	}
 	if err != nil {
 		return 0, fmt.Errorf("Write: POST to datanode (%s) failed: %v", u.String(), err)
 	}
